27197bc05cdaa9f73677808c7b0315716040171f
Карпова Ксения <ksenia.karpova.4@mail.ru>
2024-10-26 23:51:01 +0300

    инпут

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Input.css'; // Подключаем файл стилей
 
 export type InputProps = Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
   'onChange' | 'value'
 > & {
   /** Значение поля */
-  value: string;
+  value?: string;
   /** Callback, вызываемый при вводе данных в поле */
   onChange: (value: string) => void;
   /** Слот для иконки справа */
@@ -13,6 +14,39 @@ export type InputProps = Omit<
 };
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  () => null);
+  (
+    { value, onChange, afterSlot, type = 'text', className = '', disabled, placeholder, ...rest },
+    ref
+  ) => {
+    const wrapperClassNames = [
+      'input-wrapper',
+      className,
+      disabled ? 'input-disabled' : '',
+      value ? 'input-not-empty' : 'input-empty',
+    ]
+      .filter(Boolean)
+      .join(' ');
+
+    return (
+      <div className={wrapperClassNames}>
+        <input
+          {...rest} // Передаем все остальные пропсы
+          ref={ref}
+          type={type}
+          value={value || ''}
+          placeholder={placeholder}
+          onChange={(e) => onChange(e.target.value)} // Обработчик onChange
+          className="input-element"
+          disabled={disabled}
+        />
+        {afterSlot && (
+          <div className="input-after-slot">
+            {afterSlot}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 
 export default Input;
